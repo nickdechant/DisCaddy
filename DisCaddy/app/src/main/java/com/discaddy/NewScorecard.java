1bff92c36570f42430a4b0557dfa34688c7ad0c8
nkubala <nkubala@utexas.edu>
2014-10-29 19:15:35 -0500

    added functionality for creating new scorecard. NEEDS FIXED

--- DisCaddy/app/src/main/java/com/discaddy/NewScorecard.java
+++ DisCaddy/app/src/main/java/com/discaddy/NewScorecard.java
@@ -1,28 +1,45 @@
 package com.discaddy;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class NewScorecard extends Activity {
 
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_players);
-//        mDbHelper = new PlayerDbAdapter(this);
-//        mDbHelper.open();
-//        fillData();
-//        registerForContextMenu(getListView());
-//    }
-//
-//    private PlayerDbAdapter mDbHelper;
+    private Scorecard scorecard;
+    private ArrayList<String> currentPlayers;
+    private PlayerDbAdapter mDbHelper;
+    private Map<Integer, Integer> selectedPlayers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_scorecard);
+
+        this.scorecard = new Scorecard();
+        this.currentPlayers = new ArrayList<String>();
+        this.selectedPlayers = new HashMap<Integer, Integer>();
+        mDbHelper = new PlayerDbAdapter(this);
+        mDbHelper.open();
+        fillData();
     }
 
 
@@ -43,5 +60,122 @@ public class NewScorecard extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+    public void fillData() {
+        // Get all of the notes from the database and create the item list
+        Cursor c = this.mDbHelper.fetchAllPlayers();
+        startManagingCursor(c);
+
+        String[] from = new String[] { PlayerDbAdapter.KEY_Name };
+        int[] to = new int[] { R.id.text1 };
+
+        // Now create an array adapter and set it to display using our row
+        SimpleCursorAdapter players =
+                new SimpleCursorAdapter(this, R.layout.player_row, c, from, to);
+        final ListView playerList = (ListView)findViewById(R.id.players_list);
+        playerList.setAdapter(players);
+
+        playerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+//                Toast.makeText(getApplicationContext(),
+//                        "Click ListItem Number " + position, Toast.LENGTH_SHORT)
+//                        .show();
+
+                if (selectedPlayers.containsKey(position)) {
+                    if (selectedPlayers.get(position) == 1) {
+                        selectedPlayers.put(position, 0);
+                        Toast.makeText(getApplicationContext(),"item " + position + " now unselected", Toast.LENGTH_SHORT).show();
+//                        ((TextView)view).setBackgroundColor(Color.argb(0,0,0,0));
+                    }
+                    else {
+                        selectedPlayers.put(position, 1);
+                        Toast.makeText(getApplicationContext(),"item " + position + " now selected", Toast.LENGTH_SHORT).show();
+//                        ((TextView)view).setBackgroundColor(Color.argb(125, 75, 236, 90));
+                    }
+                }
+                else {
+                    selectedPlayers.put(position, 1);
+                    Toast.makeText(getApplicationContext(),"item " + position + " now selected", Toast.LENGTH_SHORT).show();
+//                    ((TextView)view).setBackgroundColor(Color.argb(125, 75, 236, 90));
+                }
+//                if (view.isSelected()) {
+//                    view.setSelected(false);
+//                if (view.isPressed()) {
+//                    view.setPressed(false);
+//                    Toast.makeText(getApplicationContext(),
+//                            "item is now unselected", Toast.LENGTH_LONG)
+//                            .show();
+////                    ((TextView)view).setBackgroundResource(Color.rgb(0,200,0));
+//                }
+//                else {
+////                    view.setSelected(true);
+//                    view.setPressed(true);
+//                    Toast.makeText(getApplicationContext(),
+//                            "item is now selected", Toast.LENGTH_LONG)
+//                            .show();
+////                    ((TextView)view).setBackgroundResource(Color.rgb(200,0,0));
+//                }
+
+
+
+                TextView tv = (TextView) view;
+                tv.setBackgroundColor(Color.argb(125, 75, 236, 90));
+
+            }
+        });
+
+
+//        playerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position,
+//                                    long id) {
+//
+//                Cursor playerData = mDbHelper.fetchPlayer(id);
+//                String name = playerData.getString(1); // Name index
+//                String course = playerData.getString(2); // Course index
+//                String score = playerData.getString(3); // Score index
+//                String disk = playerData.getString(4); // Disk index
+//
+//                Intent myIntent = new Intent(NewScorecard.this, PlayerProfile.class);
+//                myIntent.putExtra("name", name);
+//                myIntent.putExtra("course", course);
+//                myIntent.putExtra("score", score);
+//                myIntent.putExtra("disk", disk);
+//                NewScorecard.this.startActivity(myIntent);;
+//            }
+//        });
+    }
+
+    public void addNewPlayer(View view) {
+
+        //TODO: how to send user to NewPlayer activity?
+        NewPlayer newPlayer = new NewPlayer();
+        newPlayer.createNewPlayer(view);
+
+        Intent myIntent = new Intent(NewScorecard.this, Player.class);
+        NewScorecard.this.startActivity(myIntent);
+
+//        Intent myIntent = new Intent(NewPlayer.this, Player.class);
+//        NewPlayer.this.startActivity(myIntent);
+    }
+
+    public void addCurrentPlayer(View view) {
+        //get player represented by item clicked (each item should be one player)
+        //add to list of currentPlayers
+    }
+
+    public void go(View view) {
+        //currentPlayers should contain all players
+        for (Map.Entry<Integer, Integer> e : selectedPlayers.entrySet()) {
+            if (e.getValue() == 1) {
+
+            }
+        }
     }
 }
